bfc361d271d996d977a0986e78b22b2bf9861f37
Seungcode <ehrehrdltkfk@gmail.com>
2024-01-23 04:46:55 +0900

    Fix::20240123 토큰 유효성 메세지 수정

--- src/config/response.status.js
+++ src/config/response.status.js
@@ -3,7 +3,9 @@ import { StatusCodes } from "http-status-codes";
 export const status = {
     // success
     SUCCESS: {status: StatusCodes.OK, "isSuccess": true, "code": 2000, "message": "success!"},
-
+    //
+    TOKEN_IS_EXPIRED: {status: StatusCodes.TOKEN_IS_EXPIRED, "isSuccess":false, "code":419, "message": "토큰이 만료되었습니다."},
+    TOKEN_IS_INVALID: {status: StatusCodes.TOKEN_IS_INVALID, "isSuccess":false, "code":401, "message": "토큰이 유효하지 않습니다."},
     // error
     // common err
     INTERNAL_SERVER_ERROR: {status: StatusCodes.INTERNAL_SERVER_ERROR, "isSuccess": false, "code": "COMMON000", "message": "서버 에러, 관리자에게 문의 바랍니다." },
--- src/controllers/random.controller.js
+++ src/controllers/random.controller.js
@@ -2,7 +2,8 @@ import {verify} from "../service/auth.js";
 import {make_random} from "../service/random.service.js";
 
 export const randomController = async(req, res) => {
-    const user = verify(req, res => {
+try{
+    const user = await verify(req, res => {
         if (error) {
             // 에러 처리
             return res.status(500).send("Error during verification");
@@ -10,11 +11,12 @@ export const randomController = async(req, res) => {
 
         // verify 함수가 성공적으로 토큰을 확인했을 때 실행될 코드
         res.send("Verification successful");
-    });
-
+    })
     const random = await make_random(user);
     if(random===-1){
         return res.status(409).json({error: "이미 가족이 존재합니다. 탈퇴를 먼저 진행해주세요!"});
     }
     return res.status(200).json({randomToken: random});
-}
+} catch(err) {
+    return res.status(401).json({error: "유효하지 않은 토큰입니다."});
+}}
