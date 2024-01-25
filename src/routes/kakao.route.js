4084497e837bbd2fe1bba13c5fbd47d0f627ea6d
Seungcode <ehrehrdltkfk@gmail.com>
2024-01-26 02:17:36 +0900

    Fix::20240126스웨거 수정

--- src/routes/kakao.route.js
+++ src/routes/kakao.route.js
@@ -3,4 +3,13 @@ import { signInkakao, signOutKakao } from "../controllers/user.controller.js";
 
 export const kakaoRouter = express.Router();
 kakaoRouter.post('/kakao/signin', signInkakao);
+/**
+ * @swagger
+ * /api/example:
+ *   get:
+ *     description: Example endpoint
+ *     responses:
+ *       200:
+ *         description: Successful response
+ */
 kakaoRouter.post('/kakao/logout', signOutKakao);
