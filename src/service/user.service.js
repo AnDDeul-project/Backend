08f49b2bf802ecd528465795919949b57ecaa7e5
Seungcode <ehrehrdltkfk@gmail.com>
2024-01-26 00:17:18 +0900

    backup

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -6,7 +6,6 @@ import { status } from "../config/response.status.js";
 export const logOutKakao = async (kakaoToken) => {
     console.log(kakaoToken);
     
-    
     try {
         const result = await axios.post(
             "https://kapi.kakao.com/v1/user/logout",
