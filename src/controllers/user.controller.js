e58c728371b9940c6f22cc37fb0ed9654f711dd4
Seungcode <ehrehrdltkfk@gmail.com>
2024-01-27 02:31:38 +0900

    Fix::20230127 스웨거 값 추가

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,16 @@
 import { signInKakao, logOutKakao, unlinkKakao} from "../service/user.service.js";
 
 export const signInkakao = async (req, res) => {
+    try{
     console.log(req.headers);
     const headers = req.headers["authorization"];
     const kakaoToken = headers.split(" ")[1];
     const accessToken = await signInKakao(kakaoToken);
     
-    return res.status(200).json({ accessToken: accessToken });
+    return res.status(200).json({status: 200, isSuccess: true, accessToken: accessToken });
+    }catch(err){
+        return res.status(500).json({status: 500, isSuccess: false, message: "서버 에러, 관리자에게 문의 바랍니다."});
+    }
 };
 
 export const signOutKakao = async (req, res) => {
@@ -14,9 +18,9 @@ export const signOutKakao = async (req, res) => {
         const headers = req.headers["authorization"];
         const kakaoToken = headers.split(" ")[1];
         const result = await logOutKakao(kakaoToken);
-        return res.status(200).json({ message: "로그아웃 성공"});
+        return res.status(200).json({ status: 200, isSuccess: true, message: "로그아웃 성공"});
     }catch(errors){
-        return res.status(500).json({message: errors});
+        return res.status(500).json({ status: 500, isSuccess: false, message: "서버 에러, 관리자에게 문의 바랍니다."});
     }
 };
 
@@ -25,8 +29,8 @@ export const unLinkKakao = async (req, res) => {
         const headers = req.headers["authorization"];
         const kakaoToken = headers.split(" ")[1];
         const result = await unlinkKakao(kakaoToken);
-        return res.status(200).json({ message: "탈퇴 성공"});
+        return res.status(200).json({ status: 200, isSuccess: true, message: "탈퇴 성공"});
     }catch(errors){
-        return res.status(500).json({message: errors});
+        return res.status(500).json({status: 500, isSuccess: false, message: "서버 에러, 관리자에게 문의 바랍니다."});
     }
 }
--- swagger/swagger.config.js
+++ swagger/swagger.config.js
@@ -2,17 +2,23 @@ import SwaggerJsdoc from "swagger-jsdoc";
 
 const options = {
     swaggerDefinition: {
-        openapi: "3.0.0",
         info: {
-            version: "0.0.1",
-            title: 'swagger-test',
-            description: "swagger test",
+            version: "1.0.0",
+            title: '안뜰 API Swagger',
+            description: "안뜰 개발자용 스웨거 페이지입니다! 관련 문의는 백엔드 팀 아무에게나 하셔도 됩니당 다들 오늘도 홧팅:-)",
         },
         basepath: '/',
         servers: [{
             description:"안뜰 api",
             url: "http://umc-garden.store"
-        }]
+        }],
+        securityDefinitions: {
+            Authorization: {
+                type: 'apiKey',
+                in: 'header',
+                name: 'Authorization',
+            }
+        }
     },
     apis: ['./src/**/*.js', './swagger/*']
 };
