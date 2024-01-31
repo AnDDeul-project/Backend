3023339a63725fee421892eadd6d5654f38edd80
LimSeHyeon <dlatpgus1205@naver.com>
2024-01-31 16:00:19 +0900

    FIX::20240131 토큰 수정하기

--- src/controllers/check.controller.js
+++ src/controllers/check.controller.js
@@ -9,9 +9,10 @@ import { joinCheck, callCheck, updateContent, updateDate, updateComplete, delete
 
 // 리스트 추가
 export const checkadd = async (req, res) => {
+    const snsId = await verify(req, res);
     console.log("체크리스트를 추가합니다");
 
-    res.send(response(status.SUCCESS, await joinCheck(req.body)));
+    res.send(response(status.SUCCESS, await joinCheck(snsId, req.body)));
 }
 
 // 리스트 불러오기
@@ -26,6 +27,7 @@ export const checkget = async (req, res) => {
 
 // 할 일 수정
 export const checkcontent = async (req, res) => {
+    const snsId = await verify(req, res);
     console.log(`${req.params.checkid} 체크리스트가 수정되었습니다.`);
 
     res.send(response(status.SUCCESS, await updateContent(req.params.checkid, req.body)));
@@ -33,6 +35,7 @@ export const checkcontent = async (req, res) => {
 
 // 날짜 수정
 export const checkdate = async (req, res) => {
+    const snsId = await verify(req, res);
     console.log(`${req.params.checkid} 체크리스트의 날짜가 변경되었습니다.`);
 
     res.send(response(status.SUCCESS, await updateDate(req.params.checkid, req.params.date)));
@@ -40,6 +43,7 @@ export const checkdate = async (req, res) => {
 
 // 할 일 완료 혹은 완료 취소
 export const checkcomplete = async (req, res) => {
+    const snsId = await verify(req, res);
     console.log(`${req.params.checkid} 체크리스트가 완료되었습니다.`);
 
     res.send(response(status.SUCCESS, await updateComplete(req.params.checkid)));
@@ -47,6 +51,7 @@ export const checkcomplete = async (req, res) => {
 
 // 할 일 삭제
 export const checkdelete = async (req, res, next) => {
+    const snsId = await verify(req, res);
     console.log(`${req.params.checkid} 체크리스트를 삭제합니다`);
 
     res.send(response(status.SUCCESS, await deleteCheck(req.params.checkid)));
@@ -54,6 +59,7 @@ export const checkdelete = async (req, res, next) => {
 
 // 사진 업로드
 export const checkimg = async (req, res) => {
+    const snsId = await verify(req, res);
     console.log(`${req.params.checkid} 사진 업로드`);
 
     res.send(response(status.SUCCESS, await imgCheck(req.params.checkid, req.files.location)));
--- src/service/check.service.js
+++ src/service/check.service.js
@@ -7,7 +7,7 @@ import { addCheckResponseDTO, callCheckResponseDTO, contentCheckResponseDTO, dat
 import { addCheckList, getCheck, callCheckList, contentCheckList, dateCheckList, finishCheckList, deleteCheckList, imageCheckList } from "../dao/check.dao.js";
 
 // 체크리스트 추가
-export const joinCheck = async (body) => {
+export const joinCheck = async (snsId, body) => {
     const date = new Date(body.due_year, body.due_month-1, body.due_day);
     const year = date.getFullYear();
     const month = ("0" + (date.getMonth() + 1)).slice(-2); // 월을 2자리 문자열로 만들기 위해 앞에 0을 붙이고, 뒤의 2자리만 추출
@@ -15,7 +15,7 @@ export const joinCheck = async (body) => {
     const formattedDate = `${year}-${month}-${day}`; // "YYYY-MM-DD" 형식으로 날짜 문자열 생성
 
     const joinListData = await addCheckList({
-        'sender_idx' : body.sender_idx,
+        'sender_idx' : snsId,
         'receiver_idx' : body.receiver_idx,
         'due_date' : formattedDate,
         'content' : body.content
