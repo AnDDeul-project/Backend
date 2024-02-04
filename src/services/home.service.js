c331aa6e0e7ab7bbce89d009733faf9f13e16d85
Junghoon <fanatel@gmail.com>
2024-02-05 01:00:43 +0900

    CRUD 수정

--- src/services/home.service.js
+++ src/services/home.service.js
@@ -1,4 +1,4 @@
-import { createPostInDb, getPostsFromDb, getFamilyMembers } from '../dao/home.dao.js';
+import { createPostInDb, getPostsFromDb, getFamilyMembers, getPostById, updatePostById, deletePostById } from '../dao/home.dao.js';
 
 export const homeService = {
     // 게시글 작성
@@ -6,12 +6,53 @@ export const homeService = {
         // 데이터베이스에 게시글 저장
         return await createPostInDb(postData);
     },
+
     // 게시글 조회
     getPosts: async (user_idx) => {
         return await getPostsFromDb(user_idx);
     },
+
+    // 게시글 수정
+    updatePost: async (snsId, postIdx, content) => {
+        // 게시글 정보 조회
+        const post = await getPostById(post_idx);
+        
+        // 게시글이 존재하지 않는 경우
+        if (!post) {
+            throw new Error('Post not found');
+        }
+
+        // 요청한 사용자가 게시글의 소유자가 아닌 경우
+        if (post.user_idx !== snsId) {
+            throw new Error('You do not have permission to edit this post');
+        }
+
+        // 게시글 수정
+        return await updatePostById(postIdx, content);
+    },
+
+        // 게시글 삭제
+    deletePost: async (user_idx, post_idx) => {
+        // 게시글 정보 조회
+        const post = await getPostById(post_idx);
+
+        // 게시글이 존재하지 않는 경우
+        if (!post) {
+            throw new Error('Post not found');
+        }
+
+        console.log("Logged in user:", user_idx); // 로그인한 사용자의 snsId 출력 (배열 형태)
+        console.log("Post owner:", post.user_idx); // 게시글 작성자의 user_idx 출력 (문자열 형태)
+
+        // 요청한 사용자가 게시글의 소유자가 아닌 경우 (배열의 첫 번째 요소를 사용하여 비교)
+        if (post.user_idx !== user_idx[0]) { // 배열의 첫 번째 요소를 사용하여 비교
+            throw new Error('You do not have permission to delete this post');
+        }
+        // 게시글 삭제
+        return await deletePostById(post_idx);
+    },
     // 가족 구성원 조회
-    getFamilyMembers: async (user_idx) => {
+    getFamilyMembers: async (user_idx) => { 
         return await getFamilyMembers(user_idx);
     }
 };
