cdc46da1f7f41e804d32a777d9bccab12e0155f4
LimSeHyeon <dlatpgus1205@naver.com>
2024-02-13 22:56:45 +0900

    FIX::20240213 체크리스트 코드 재작성

--- src/controllers/check.controller.js
+++ src/controllers/check.controller.js
@@ -1,96 +1,103 @@
-// check.controller.js
+//check.controller.js
 
-import { response } from '../config/response.js';
-import { status } from '../config/response.status.js';
 import { verify } from '../service/auth.js';
+import { writeCheckService, callCheckService, contentCheckService, dateCheckService, completeCheckService, deleteCheckService, imgCheckService } from '../service/check.service.js';
 
-
-import { joinCheck, callCheck, updateContent, updateDate, updateComplete, deleteCheck, imgCheck } from "../service/check.service.js";
-
-// 리스트 추가
-export const checkadd = async (req, res, next) => {
-    try {
-        const snsId = await verify(req, res);
-        console.log("체크리스트를 추가합니다");
-        res.send(response(status.SUCCESS, await joinCheck(snsId, req.body)));
-    } catch (err) {
-        next(err);
-    }
-}
-
-// 리스트 불러오기
-export const checkget = async (req, res, next) => {
+// 체크리스트 추가
+export const addCheck = async (req, res) => {
     let snsId;
     try {
         snsId = await verify(req, res);
-        snsId = req.params.mode==false ? snsId[0] : req.params.userid;//0이면 내거 1이면 다른사람거
-        console.log(`${snsId}의 ${req.params.date} 체크리스트 목록을 불러옵니다`);
-        res.send(response(status.SUCCESS, await callCheck(snsId, req.params.date)));
     } catch (err) {
-        next(err);
+        return res.status(401).json({status: 401, isSuccess: false, error: "유효하지 않은 토큰입니다."})
     }
+    console.log("체크리스트를 추가합니다");
+    const result = await writeCheckService(snsId, req.body);
+    return res.status(200).json({status: 200, isSuccess: true, check: result});
 }
 
-// 할 일 수정
-export const checkcontent = async (req, res, next) => {
+// 체크리스트 목록 불러오기
+export const getCheck = async (req, res) => {
+    let snsId;
+    try {
+        console.log(1);
+        snsId = await verify(req, res);
+    } catch (err) {
+        return res.status(401).json({status: 401, isSuccess: false, error: "유효하지 않은 토큰입니다."})
+    }
+    
+    snsId = req.params.mode==false ? snsId[0] : req.params.userid;//0이면 내거 1이면 다른사람거
+    if(req.params.mode==1) snsId = req.params.userid;
+    console.log("사용자의 체크리스트를 불러옵니다");
+    const result = await callCheckService(snsId, req.params.date);
+    if(result==-1)
+        return res.status(451).json({status:451, isSuccess: false, error: "해당 날짜에 만들어진 체크리스트가 없습니다"});
+    return res.status(200).json({status: 200, isSuccess: true, checklist: result});
+}
+
+// 체크리스트 내용 수정
+export const contentCheck = async (req, res) => {
     let snsId;
     try {
         snsId = await verify(req, res);
-        console.log(`${req.params.checkid} 체크리스트가 수정되었습니다.`);
-        res.send(response(status.SUCCESS, await updateContent(req.params.checkid, req.body)));
     } catch (err) {
-        next(err);
+        return res.status(401).json({status: 401, isSuccess: false, error: "유효하지 않은 토큰입니다."})
     }
+    console.log("체크리스트 내용을 수정합니다");
+    const result = await contentCheckService(req.params.checkid, req.body.content);
+    return res.status(200).json({status: 200, isSuccess: true, check: result});
 }
 
-// 날짜 수정
-export const checkdate = async (req, res, next) => {
+// 체크리스트 기한 변경
+export const dateCheck = async (req, res) => {
     let snsId;
     try {
         snsId = await verify(req, res);
-        console.log(`${req.params.checkid} 체크리스트의 날짜가 변경되었습니다.`);
-        res.send(response(status.SUCCESS, await updateDate(req.params.checkid, req.params.date)));
     } catch (err) {
-        next(err);
+        return res.status(401).json({status: 401, isSuccess: false, error: "유효하지 않은 토큰입니다."})
     }
+    console.log("체크리스트 기한을 수정합니다");
+    const result = await dateCheckService(req.params.checkid, req.params.date);
+    return res.status(200).json({status: 200, isSuccess: true, check: result});
 }
 
-// 할 일 완료 혹은 완료 취소
-export const checkcomplete = async (req, res, next) => {
+// 체크리스트 완료여부 변경
+export const completeCheck = async (req, res) => {
     let snsId;
     try {
         snsId = await verify(req, res);
-        console.log(`${req.params.checkid} 체크리스트가 완료되었습니다.`);
-        res.send(response(status.SUCCESS, await updateComplete(req.params.checkid)));
     } catch (err) {
-        next(err);
+        return res.status(401).json({status: 401, isSuccess: false, error: "유효하지 않은 토큰입니다."})
     }
+    console.log("체크리스트 완료여부를 수정합니다");
+    const result = await completeCheckService(req.params.checkid);
+    return res.status(200).json({status: 200, isSuccess: true, check: result});
 }
 
-// 할 일 삭제
-export const checkdelete = async (req, res, next) => {
+// 체크리스트 삭제
+export const deleteCheck = async (req, res) => {
     let snsId;
     try {
         snsId = await verify(req, res);
-        console.log(`${req.params.checkid} 체크리스트를 삭제합니다`);
-        res.send(response(status.SUCCESS, await deleteCheck(req.params.checkid)));
     } catch (err) {
-        next(err);
+        return res.status(401).json({status: 401, isSuccess: false, error: "유효하지 않은 토큰입니다."})
     }
+    console.log("체크리스트를 삭제합니다");
+    const result = await deleteCheckService(req.params.checkid);
+    return res.status(200).json({status: 200, isSuccess: true, message: "체크리스트를 삭제했습니다"});
 }
 
-// 사진 업로드
-export const checkimg = async (req, res, next) => {
+// 체크리스트 인증샷 추가
+export const imgCheck = async (req, res) => {
     let snsId;
     try {
         snsId = await verify(req, res);
-        console.log(`${req.body.checkid} 사진 업로드`);
-        const result = await imgCheck(req.body.checkid, req.file.location);
-        if(result==-1) {
-            return res.status(450).json({status:450, isSuccess: false, error: "체크리스트 달성 전에는 인증샷을 추가할 수 없습니다"});
-        }
-        else res.send(response(status.SUCCESS, result));
     } catch (err) {
-        next(err);
+        return res.status(401).json({status: 401, isSuccess: false, error: "유효하지 않은 토큰입니다."})
     }
+    console.log("체크리스트에 이미지를 추가합니다");
+    const result = await imgCheckService(req.body.checkid, req.file.location);
+    if(result==-1)
+        return res.status(450).json({status:450, isSuccess: false, error: "체크리스트 달성 전에는 인증샷을 추가할 수 없습니다"});
+    else return res.status(200).json({status: 200, isSuccess: true, check: result});
 }
--- src/service/check.service.js
+++ src/service/check.service.js
@@ -1,76 +1,52 @@
 //check.service.js
 
-import { BaseError } from "../config/error.js";
-import { status } from "../config/response.status.js";
-
-import { addCheckResponseDTO, callCheckResponseDTO, contentCheckResponseDTO, dateCheckResponseDTO, completeCheckResponseDTO, deleteCheckResponseDTO, imgCheckResponseDTO } from "../dto/check.dto.js"
-import { addCheckList, getCheck, modifyCheckList, callCheckList, contentCheckList, dateCheckList, finishCheckList, deleteCheckList, imageCheckList } from "../dao/check.dao.js";
-import { find_member } from "../dao/family.dao.js";
+import { getOne, addOne, getAll, changeContent, changeDate, changeComplete, removeOne, putImg } from '../dao/check.dao.js';
 
 // 체크리스트 추가
-export const joinCheck = async (snsId, body) => {
-    const date = new Date(body.due_year, body.due_month-1, body.due_day);
-    const year = date.getFullYear();
-    const month = ("0" + (date.getMonth() + 1)).slice(-2); // 월을 2자리 문자열로 만들기 위해 앞에 0을 붙이고, 뒤의 2자리만 추출
-    const day = ("0" + date.getDate()).slice(-2); // 일을 2자리 문자열로 만들기 위해 앞에 0을 붙이고, 뒤의 2자리만 추출
-    const formattedDate = `${year}-${month}-${day}`; // "YYYY-MM-DD" 형식으로 날짜 문자열 생성
-
-    const joinListData = await addCheckList({
-        'sender_idx' : snsId[0],
-        'receiver_idx' : body.receiver_idx,
-        'due_date' : formattedDate,
-        'content' : body.content
-    });
-
-    const result = await modifyCheckList(await getCheck(joinListData)); 
-    /*
-    const sender = await find_member(result[0].sender);
-    const receiver = await find_member(result[0].receiver);
-    result[0].sender = sender;
-    result[0].receiver = receiver;
-    */
-    console.log(result);
-
+export const writeCheckService = async(snsid, body) => {
+    const checkid = await addOne(snsid, body);
+    const result = await getOne(checkid);
     return result;
 }
 
-// 체크리스트 불러오기
-export const callCheck = async (userid, date) => {
-    const check = await callCheckList(userid, date);
-    let sender = [];
-    for(let i = 0; i < check.length; i++) {
-        sender.push(await find_member(check[i].sender_idx));
-    }
-    return callCheckResponseDTO(check, sender);
+// 체크리스트 목록 불러오기
+export const callCheckService = async(snsid, date) => {
+    const result = await getAll(snsid, date);
+    if(result==-1) return -1;
+    return result;
 }
 
-// 할 일 수정
-export const updateContent = async (checkid, body) => {
-    await contentCheckList(checkid, body.content);
-    return contentCheckResponseDTO(await modifyCheckList(await getCheck(checkid)));
+// 체크리스트 내용 수정
+export const contentCheckService = async(checkid, content) => {
+    await changeContent(checkid, content);
+    const result = await getOne(checkid);
+    return result;
 }
 
-// 할 일 날짜 수정
-export const updateDate = async (checkid, date) => {
-    await dateCheckList(checkid, date);
-    return dateCheckResponseDTO(await modifyCheckList(await getCheck(checkid, date)));
+// 체크리스트 기한 변경
+export const dateCheckService = async(checkid, date) => {
+    await changeDate(checkid, date);
+    const result = await getOne(checkid);
+    return result;
 }
 
-// 할 일 완료 혹은 완료 취소
-export const updateComplete = async (checkid) => {
-    await finishCheckList(checkid);
-    return completeCheckResponseDTO(await modifyCheckList(await getCheck(checkid)));
+// 체크리스트 완료여부 변경
+export const completeCheckService = async(checkid) => {
+    await changeComplete(checkid);
+    const result = await getOne(checkid);
+    return result;
 }
 
-// 할 일 삭제
-export const deleteCheck = async (checkid) => {
-    const check = await deleteCheckList(checkid);
-    if(check==1) return deleteCheckResponseDTO(check);
+// 체크리스트 삭제
+export const deleteCheckService = async(checkid) => {
+    await removeOne(checkid);
+    return;
 }
 
-// 사진 업로드
-export const imgCheck = async (checkid, location) => {
-    const result = await imageCheckList(checkid, location);
-    if(result==1) return imgCheckResponseDTO(await modifyCheckList(await getCheck(checkid)));
-    else if(result == -1) return -1;
+// 체크리스트 이미지 넣기
+export const imgCheckService = async(checkid, location) => {
+    const canPut = await putImg(checkid, location);
+    if(canPut==-1) return -1;
+    const result = await getOne(checkid);
+    return result;
 }
