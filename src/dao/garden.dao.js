4259621e5d45123120d01a0be21861663e3dc9f4
LimSeHyeon <dlatpgus1205@naver.com>
2024-02-17 00:40:26 +0900

    FIX::20240216 정원 사랑주기 로직 수정

--- src/dao/garden.dao.js
+++ src/dao/garden.dao.js
@@ -3,7 +3,7 @@ import { BaseError } from "../config/error.js";
 import { status } from "../config/response.status.js";
 
 
-const ranges = [0.2, 0.4, 0.6, 0.8, 1];
+const ranges = [0, 0.2, 0.4, 0.6, 0.8, 1];
 const images = ['img_0', 'img_1', 'img_2', 'img_3', 'img_4', 'img_5'];
 
 
@@ -33,7 +33,7 @@ export const getOne = async(snsid) => {
                 break;
             }
         }
-        const result3 = await pool.query(`SELECT name, ${img} FROM flower WHERE idx = ?`, num);
+        const result3 = await pool.query(`SELECT name, ${img} FROM flower WHERE idx = (?, ?)`, [num,17]);
         conn.release();
         //꽃 번호, 포인트, 이름, 그림
         return {idx: num, point: point, name: result3[0].name, img: result3[0][img]};
@@ -55,28 +55,35 @@ export const cal_point = async(snsid) => {
         await pool.query("UPDATE user SET point = point - 2 WHERE snsId = ?", snsid);
         //가족 꽃번호, 포인트, required 불러와서 f_point+2랑 비교한 뒤에 userfam 업데이트
         const result = await pool.query("SELECT family_code FROM user WHERE snsId = ?", snsid);
+        console.log(result[0][0].family_code);
         if(result[0][0].family_code==null) {
             return -2;
         }
-        const result2 = await pool.query("SELECT f_num, f_point FROM userfam WHERE family_code = ?", result[0][0].family_code);
-        const result3 = await pool.query("SELECT required FROM flower WHERE idx = ?", result2[0][0].f_num);
+        const fam = await pool.query("SELECT f_num, f_point FROM userfam WHERE family_code = ?", result[0][0].family_code);
+        const req = await pool.query("SELECT required FROM flower WHERE idx = ?", fam[0][0].f_num);
         
         //포인트 다 채우면 꽃 바꾸고 포인트 0으로, 다 안 채웠으면 그냥 +2   
-        if(result2[0].f_point +2 >= result3[0].required) {
+        if(fam[0][0].f_point +2 >= req[0][0].required) {
             //set f_num = f_num+1 있었던 것
             await pool.query("UPDATE userfam SET f_point = 0 WHERE family_code = ?", result[0][0].family_code);
+            fam[0][0].f_point = 0;
         } else {
             await pool.query("UPDATE userfam SET f_point = f_point + 2 WHERE family_code = ?", result[0][0].family_code);
         }
         //이미지 불러와 이거 반환할거야
         let img;
-        for(let i = 0; i < ranges.length; i++) {
-            if(result2[0][0].f_point < ranges[i]*result3[0][0].required || point[0][0].point===result3[0][0].standard) {
-                img = images[i];
-                break;
+        if(fam[0][0].f_point===req[0][0].required) {
+            img = images[5];
+        } else {
+            for(let i = 0; i < ranges.length; i++) {
+                if(fam[0][0].f_point < ranges[i]*req[0][0].required) {
+                    img = images[i];
+                    break;
+                }
             }
         }
-        const [result4] = await pool.query(`SELECT ${img} FROM flower WHERE idx = ? || ?`, [result2[0][0].f_num, 17]);
+        console.log(img);
+        const [result4] = await pool.query(`SELECT ${img} FROM flower WHERE idx IN (?,?)`, [fam[0][0].f_num, 17]);
         result4[1].gauge = result4[1][img];
         delete result4[1][img];
         conn.release();
