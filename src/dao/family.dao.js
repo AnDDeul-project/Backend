1af38aa46e5953ae66f769c8986055a2917f9396
Seungcode <ehrehrdltkfk@gmail.com>
2024-02-18 03:17:43 +0900

    Fix::알람기능 수정

--- src/dao/family.dao.js
+++ src/dao/family.dao.js
@@ -2,18 +2,14 @@ import { pool } from "../config/db.connect.js";
 import { BaseError } from "../config/error.js";
 import moment from 'moment-timezone';
 import { status } from "../config/response.status.js";
-import moment from 'moment-timezone';
 
 export const check_leader  = async(user_id) => {
     try{
-        //const conn = await pool.getConnection();
         const result = await pool.query("SELECT family_code FROM userfam WHERE user_idx = ?", user_id);
-        //conn.release();
         console.log(result[0].length);
         if(result[0].length > 0){
             return -1;
         }
-        console.log("Here");
         return 1;
     }catch(e){
         throw new BaseError(status.PARAMETER_IS_WRONG, e);
@@ -32,7 +28,6 @@ export const delete_member  = async(user_id) => {
 export const family = async(family_code) => {
     try{
         const result = await pool.query("SELECT user_idx FROM userfam WHERE family_code = ?", family_code);
-        console.log(result);
         if(result[0][0] != null){
             return result[0][0].user_idx;
         }
@@ -45,7 +40,6 @@ export const family = async(family_code) => {
 export const find_member = async(user_id) => {
     try{
         const result = await pool.query("SELECT nickname FROM user WHERE snsId = ?", user_id);
-        console.log(result);
         return result[0][0].nickname;
     }catch(e){
         throw new BaseError(status.PARAMETER_IS_WRONG, e);
@@ -63,11 +57,10 @@ export const family_info = async(family_code) => {
 }
 export const add_family = async(user_id, family_code, user_name, now_user) => {
     try{
-        const currentDate = moment().tz('Asia/Seoul').format('YYYY-MM-DD');
         const content = user_id + "님이 가족 신청을 보냈습니다! 지금 확인하고 반겨주세요:)";
         const alarmDate = moment().tz('Asia/Seoul').format('YYYY-MM-DD HH:mm:ss');
         await pool.query("UPDATE user SET family_code = ? , auth=0 , point = 0 WHERE snsId = ?", [family_code, now_user]);
-        await pool.query("INSERT INTO alram (user_idx, checked, content, creat_at, place) VALUES (?, ?, ?, ?, ?)", [user_name, 0, content, currentDate, 'family']);
+        await pool.query("INSERT INTO alram (user_idx, checked, content, creat_at, place) VALUES (?, ?, ?, ?, ?)", [user_name, 0, content, alarmDate, 'family']);
         return;
     }catch(e){
         throw new BaseError(status.PARAMETER_IS_WRONG, e);
