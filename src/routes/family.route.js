6750d62ccf27280434e1235fcaafe084bd3bdb56
Seungcode <ehrehrdltkfk@gmail.com>
2024-07-14 20:10:44 +0900

    240714::Fix 탈퇴기능 수정

--- src/routes/family.route.js
+++ src/routes/family.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { familyController, deleteController, getinfoController, requestController } from "../controllers/family.controller.js";
+import { familyController,updateController, deleteController, getinfoController, requestController } from "../controllers/family.controller.js";
 
 export const familyRoute = express.Router();
 familyRoute.put('/add', familyController);
 familyRoute.get('/request', requestController);
 familyRoute.get('/info/:family_code', getinfoController);
 familyRoute.delete('/delete', deleteController);
+familyRoute.patch('/leader/:userid', updateController);
