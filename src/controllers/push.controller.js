787d8947734930f0b75be14b6d2eeb400b120df7
LimSeHyeon <dlatpgus1205@naver.com>
2024-07-20 18:34:24 +0900

    20240720 update: 바뀐 디바이스 토큰값 반환값에 추가

--- src/controllers/push.controller.js
+++ src/controllers/push.controller.js
@@ -16,5 +16,5 @@ export const putToken = async (req, res) => {
     return res
       .status(405)
       .json({ status: 403, isSuccess: false, error: "토큰 입력 실패" }); //나중에 에러코드 수정하기
-  return res.status(200).json({ status: 200, isSuccess: true });
+  return res.status(200).json({ status: 200, isSuccess: true, updatedToken: result});
 };
--- src/dao/push.dao.js
+++ src/dao/push.dao.js
@@ -4,8 +4,9 @@ import { status } from "../config/response.status.js";
 
 export const putToken = async(snsId, token) => {
     try {
-        let result = await pool.query("UPDATE user SET device_token = ? WHERE snsId = ?", [token, snsId]);
-        return result[0];
+        await pool.query("UPDATE user SET device_token = ? WHERE snsId = ?", [token, snsId]);
+        const [result] = await pool.query("SELECT device_token FROM user WHERE snsId = ?", [snsId]);
+        return result[0].device_token;
     }catch(e){
         console.log(err);
         throw new BaseError(status.PARAMETER_IS_WRONG, e);
