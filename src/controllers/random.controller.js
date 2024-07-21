7f5a111e4d3f544d41d60550ef02aa221816a1a7
Seungcode <ehrehrdltkfk@gmail.com>
2024-07-22 00:56:22 +0900

    fix::make_family

--- src/controllers/random.controller.js
+++ src/controllers/random.controller.js
@@ -4,7 +4,7 @@ import {make_random} from "../service/random.service.js";
 export const randomController = async(req, res) => {
     let user;
     try {
-        await verify(req, res);
+        user = await verify(req, res);
     } catch (err) {
         return;
     }
