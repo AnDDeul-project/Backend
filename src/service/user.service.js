cb71d1af5b0f52d7c628a277a0d3c5f786033a4d
Seungcode <ehrehrdltkfk@gmail.com>
2024-07-22 13:30:28 +0900

    Fix::login

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -67,33 +67,33 @@ export const unlinkKakao = async (kakaoToken, content) => {
 }
 
 export const signInKakao = async (kakaoToken) => {
-        const result = await axios.get("https://kapi.kakao.com/v2/user/me", {
-            headers: {
-                Authorization: `Bearer ${kakaoToken}`,
-            },
-        });
-    
-        const {data} = result
-        const nickname = data.properties.nickname;
-        const email = data.kakao_account.email;
-        const snsId = data.id;
-        const image = data.properties.profile_image;
-        const providerType = "kakao";
-        if (!nickname || !email || !snsId) throw new BaseError(status.BAD_REQUEST);
-    
-        const user = await findUser(snsId);
-        if (user==-1) {
-            await createUser({
-                'email': email, 
-                'nickname': nickname, 
-                'snsId': snsId, 
-                'image': image, 
-                'providerType': providerType
-            });
-     
-            return [jwt.sign({ kakao_id: data.id }, process.env.KAKAO_ID, {expiresIn: 864000}), snsId];
+    const result = await axios.get("https://kapi.kakao.com/v2/user/me", {
+        headers: {
+            Authorization: `Bearer ${kakaoToken}`,
+        },
+    });
 
-        };   
+    const {data} = result
+    const nickname = data.properties.nickname;
+    const email = data.kakao_account.email;
+    const snsId = data.id;
+    const image = data.properties.profile_image;
+    const providerType = "kakao";
+    if (!nickname || !email || !snsId) throw new BaseError(status.BAD_REQUEST);
+
+    const user = await findUser(snsId);
+    if (user==-1) {
+        await createUser({
+            'email': email, 
+            'nickname': nickname, 
+            'snsId': snsId, 
+            'image': image, 
+            'providerType': providerType
+        });
+    }; 
+    
+    
+    return [jwt.sign({ kakao_id: data.id }, process.env.KAKAO_ID, {expiresIn: 864000}), snsId];
 }
 
 
