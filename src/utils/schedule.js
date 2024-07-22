60033fb80579d4766473477fb7e690af1e1adc9e
LimSeHyeon <dlatpgus1205@naver.com>
2024-07-22 14:50:10 +0900

    20240722 feat: 체크리스트 마감 알림 전송 후 알림 테이블에 추가

--- src/utils/schedule.js
+++ src/utils/schedule.js
@@ -6,14 +6,15 @@ import { pushAlarm } from "../service/push.service.js";
 
 export const checkComplete = async () => {
     const executeJob = async () => {
-        const currentDate = moment("2024.07.20").tz("Asia/Seoul").format('YYYY-MM-DD');
-        const [receivers] = await pool.query(`SELECT DISTINCT receiver_idx FROM checklist WHERE complete = 0 AND due_date = ?`, [currentDate]);
+        const current = moment().tz("Asia/Seoul").format('YYYY-MM-DD HH:mm:ss');
+        const today = moment(current).format('YYYY-MM-DD');
+        const [receivers] = await pool.query(`SELECT DISTINCT receiver_idx FROM checklist WHERE complete = 0 AND due_date = ?`, [today]);
         for( const receiver of receivers ) {
             const [receiverToken] = await pool.query("SELECT device_token FROM user WHERE snsId = ?", [receiver.receiver_idx]);
             const deviceToken = receiverToken[0].device_token;
             const alarm_content = "아직 못한 일이 남아있어요";
-            console.log(deviceToken);
             pushAlarm(alarm_content, deviceToken);
+            await pool.query("INSERT INTO alarm (user_idx, checked, content, create_at, place) VALUES (?, ?, ?, ?, ?)", [receiver.receiver_idx, 0, alarm_content, current, 'checklist']);
         }
     };
 
